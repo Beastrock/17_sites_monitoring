cc22dbddfa9980e81c6a1c80ceef353bd27254da
Beastrock <beastrock@mail.ru>
2017-01-13 16:44:50 +0300

    Add first script version, readme and requirements.

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Script monitors sites health: it parses txt file with urls, checks  if server respond with 200 status code and if expiration date was paid month ahead and then prints statistic table.  
 
-# Project Goals
+## Getting Started  
+
+Clone repository:   
+`$ git clone https://github.com/Beastrock/17_sites_monitoring.git`    
+
+## Usage
+ 
+`python format_price.py -f <txt file with urls for checking` 
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,69 @@
-def load_urls4check(path):
-    pass
+import whois
+import datetime
+import argparse
+import os.path
+from requests import get
+from requests import exceptions
+from urllib.parse import urlparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Site monitoring utility")
+    parser.add_argument("-f", dest="filepath",
+                        help="filepath to urls txt file", type=str, required=True)
+    return parser.parse_args()
+
+
+def load_urls_list_from_file(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, "r", encoding="utf-8") as file_handler:
+        return file_handler.read().split()
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return get(url, timeout=10).status_code == 200
+    except exceptions.ConnectionError:
+        return None
 
-def get_domain_expiration_date(domain_name):
-    pass
 
-if __name__ == '__main__':
-    pass
+def is_expiration_date_paid_status(url, min_days_left_to_expiration_date=30):
+    try:
+        expiration_date_responce = whois.whois(urlparse(url).netloc).expiration_date
+    except whois.parser.PywhoisError:
+        expiration_date_responce = None
+    if isinstance(expiration_date_responce, datetime.datetime):
+        expiration_date = expiration_date_responce.date()
+    elif isinstance(expiration_date_responce, list):
+        expiration_date = expiration_date_responce[0].date()
+    else:
+        return None
+    todays_date = datetime.datetime.now().date()
+    days_till_domain_expiration = (expiration_date - todays_date).days
+    return days_till_domain_expiration > min_days_left_to_expiration_date
+
+
+def get_urls_sites_statistic(urls):
+    expiration_date_statistic = ["PASSED" if is_expiration_date_paid_status(url)
+                                 else "FAILED" for url in urls]
+    server_respond_statistic = ["PASSED" if is_server_respond_with_200(url)
+                                else "FAILED" for url in urls]
+    return [(urlparse(url).netloc, expiration_date_status, server_respond_status)
+            for url, expiration_date_status, server_respond_status
+            in zip(urls, expiration_date_statistic, server_respond_statistic)]
+
+
+def output_statistic_to_the_console(statistic):
+    print("|{:^21}|{:^22}|{:^25}|".
+          format("SERVER RESPOND STATUS", "EXPIRATION DATE STATUS", "DOMAIN"))
+    for domain, expiration_status, server_respond_status in statistic:
+        print("|{:^21}|{:^22}| {:24}|".
+              format(server_respond_status, expiration_status, domain))
+
+
+if __name__ == "__main__":
+    args = get_args()
+    urls = load_urls_list_from_file(args.filepath)
+    statistics = get_urls_sites_statistic(urls)
+    output_statistic_to_the_console(statistics)
